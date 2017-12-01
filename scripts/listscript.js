06ba88dbb9bf4c5717c24948c5ccfab7ac6bd806
Erik Morrow <cyborgpencil@gmail.com>
2017-12-01 16:33:12 -0700

    working on deleting list

--- scripts/listscript.js
+++ scripts/listscript.js
@@ -1,5 +1,13 @@
+var ul = document.createElement("ul");
+ul.id = "ul_List";
+var currentItemList = [];
+
+function LoadPage(){
+  document.getElementById('list_location').appendChild(ul);
+}
 
 function AddListItem(){
+
   // Get currrent text
   var currentTextList = document.getElementById("item_text").value;
 
@@ -8,13 +16,37 @@ function AddListItem(){
   {
     // Create LI Object
     var li = document.createElement("li");
-    li.innerHTML = currentTextList +"  <input type='button' value='delete' />";
+
+
+    //if(ul.children.length > 0 || ul.children.length != undefined){
+      //id_num = ul.children.length-1;
+    //}
+  //  else {
+    //id_num = 0;
+    //}
+    li.innerHTML = currentTextList+"  <input type='button' value='delete' onclick='deleteListItem(this.parentNode)'/>";
     //document.create
-    document.getElementById("ul_List").appendChild(li);
-    // Debug
-    console.log(li);
+    //currentItemList.push(li);
+    //ul.innerHTML = "";
+    //for (var i = 0; i < currentItemList.length; i++) {
+      ul.appendChild(li);
+    //}
   }
 
   // Clear textbox
   document.getElementById("item_text").value = null;
+}
+
+function deleteListItem(item)
+{
+
+  //currentItemList.pondex);
+  //item.parentNode.parentNode.removeChild(item.parentNode);
+  //console.log(item.parentNode.parentNode.parentNode.children);
+  //console.log(item.parentNode.children.length);
+  for (var i = 0; i < ul.children.length; i++) {
+    document.getElementById("ul_List").removeChild();
+  }
+
+  //console.log(item);
 }
